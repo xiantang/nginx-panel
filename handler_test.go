a9bb1ac19040221dae698155f28aa150cf66a64c
jingdi zhu <zhujingdi1998@gmail.com>
2022-09-07 23:30:26 +0800

    update config

--- handler_test.go
+++ handler_test.go
@@ -10,7 +10,7 @@ import (
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
-func Test_handleSubmit(t *testing.T) {
+func Test_handleCURD(t *testing.T) {
 	t.Setenv("SKIP_NGINX_TEST", "true")
 	req := NginxReq{
 		ConfigBody: "server {\\nlisten 127.0.0.1:8089;\\n# Additional server configuration\\nlocation /some/path/ {\\nreturn 200;\\n}\\n}",
@@ -36,4 +36,25 @@ func Test_handleSubmit(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(resp.Kvs))
 	assert.Equal(t, req.ConfigBody, string(resp.Kvs[0].Value))
+
+	// list config
+	t.Log("list config")
+	bodys, err := handleList(context.Background())
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(bodys))
+
+	// get config
+	t.Log("get config")
+	body, err := handleGet(context.Background(), req.SererName)
+	assert.NoError(t, err)
+	assert.Equal(t, req.ConfigBody, body)
+
+	// delete config
+	t.Log("delete config")
+	err = handleDelete(context.Background(), req.SererName)
+	assert.NoError(t, err)
+	resp, err = cli.Get(ctx, "/config/shopee.com")
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(resp.Kvs))
+
 }
